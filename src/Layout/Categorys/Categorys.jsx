a8e48fa6d1c3bd223fb86315fc389d51f443d65d
MehediHasan4321 <126mehedi@gmail.com>
2023-05-23 22:59:53 +0600

    fix some data loading problem

--- src/Layout/Categorys/Categorys.jsx
+++ src/Layout/Categorys/Categorys.jsx
@@ -1,19 +1,24 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext } from 'react';
 import CetegoryCart from './CetegoryCart';
 
 import { AuthContext } from '../../AuthProvider/AuthProvider';
+import Loading from '../ShareCompo/Loading/Loading';
 
 const Categorys = () => {
     const {banners} = useContext(AuthContext)
    
-    return (
-        <div className='container mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 my-24'>
-            
-            {
-                banners.map(cate=><CetegoryCart key={cate._id} cate={cate}/>)
-            }
-        </div>
-    );
+    if(banners){
+        return (
+            <div className='container mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 my-24'>
+                
+                {
+                    banners.map(cate=><CetegoryCart key={cate._id} cate={cate}/>)
+                }
+            </div>
+        );
+    }else{
+        return <Loading/>
+    }
 };
 
 export default Categorys;
--- src/Layout/ImgGallary/ImgGallary.jsx
+++ src/Layout/ImgGallary/ImgGallary.jsx
@@ -1,5 +1,3 @@
-import React, { useEffect, useState } from 'react';
-
 
 const ImgGallary = () => {
     
@@ -8,28 +6,28 @@ const ImgGallary = () => {
             <h1 className='text-center text-4xl font-semibold'>Our Images Gallary</h1>
             <div className='grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 mt-24'>
                 <div className='w-[370px] h-80 m-2 bg-[#F2F2F2]' data-aos="fade-up" data-aos-duration="1000">
-                    <img className='w-full h-full p-2' src={'https://m.media-amazon.com/images/I/61w7TOn0YyL.jpg'} alt="Gallary Images" />
+                    <img className='w-full h-full p-2' src='https://m.media-amazon.com/images/I/61w7TOn0YyL.jpg' alt="Gallary Images" />
                 </div>
                 <div className='w-[370px] h-80 m-2 bg-[#F2F2F2]' data-aos="fade-up" data-aos-duration="1000">
-                    <img className='w-full h-full p-2' src={'https://m.media-amazon.com/images/I/61VSQNCI4RL._SX466_.jpg'} alt="Gallary Images" />
+                    <img className='w-full h-full p-2' src="https://m.media-amazon.com/images/I/61VSQNCI4RL._SX466_.jpg" alt="Gallary Images" />
                 </div>
                 <div className='w-[370px] h-80 m-2 bg-[#F2F2F2]' data-aos="fade-up" data-aos-duration="1000">
-                    <img className='w-full h-full p-2' src={'https://m.media-amazon.com/images/I/91iUn9jPMYL._AC_UF894,1000_QL80_.jpg'} alt="Gallary Images" />
+                    <img className='w-full h-full p-2' src="https://m.media-amazon.com/images/I/91iUn9jPMYL._AC_UF894,1000_QL80_.jpg" alt="Gallary Images" />
                 </div>
                 <div className='w-[370px] h-80 m-2 bg-[#F2F2F2]' data-aos="fade-up" data-aos-duration="1000">
-                    <img className='w-full h-full p-2' src={'https://m.media-amazon.com/images/I/81vNqNswHGL._SL1500_.jpg'} alt="Gallary Images" />
+                    <img className='w-full h-full p-2' src="https://m.media-amazon.com/images/I/81vNqNswHGL._SL1500_.jpg" alt="Gallary Images" />
                 </div>
                 <div className='w-[370px] h-80 m-2 bg-[#F2F2F2]' data-aos="fade-up" data-aos-duration="1000">
-                    <img className='w-full h-full p-2' src={'https://m.media-amazon.com/images/I/61Y1shuHJsL._AC_UF894,1000_QL80_.jpg'} alt="Gallary Images" />
+                    <img className='w-full h-full p-2' src="https://m.media-amazon.com/images/I/61Y1shuHJsL._AC_UF894,1000_QL80_.jpg" alt="Gallary Images" />
                 </div>
                 <div className='w-[370px] h-80 m-2 bg-[#F2F2F2]' data-aos="fade-up" data-aos-duration="1000">
-                    <img className='w-full h-full p-2' src={'https://m.media-amazon.com/images/I/81GOmHTaSWL._SL1500_.jpg'} alt="Gallary Images" />
+                    <img className='w-full h-full p-2' src="https://m.media-amazon.com/images/I/81GOmHTaSWL._SL1500_.jpg" alt="Gallary Images" />
                 </div>
                 <div className='w-[370px] h-80 m-2 bg-[#F2F2F2]' data-aos="fade-up" data-aos-duration="1000">
-                    <img className='w-full h-full p-2' src={'https://m.media-amazon.com/images/I/81GOmHTaSWL._SL1500_.jpg'} alt="Gallary Images" />
+                    <img className='w-full h-full p-2' src="https://m.media-amazon.com/images/I/81GOmHTaSWL._SL1500_.jpg" alt="Gallary Images" />
                 </div>
                 <div className='w-[370px] h-80 m-2 bg-[#F2F2F2]' data-aos="fade-up" data-aos-duration="1000">
-                    <img className='w-full h-full p-2' src={'https://m.media-amazon.com/images/I/81GOmHTaSWL._SL1500_.jpg'} alt="Gallary Images" />
+                    <img className='w-full h-full p-2' src="https://m.media-amazon.com/images/I/81GOmHTaSWL._SL1500_.jpg" alt="Gallary Images" />
                 </div>
                 
             </div>
