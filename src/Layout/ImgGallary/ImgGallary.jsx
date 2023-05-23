80f7f322e82e28ec4a6642e3e32ee3071249a0cd
MehediHasan4321 <126mehedi@gmail.com>
2023-05-23 23:07:19 +0600

    img gallary update

--- src/Layout/ImgGallary/ImgGallary.jsx
+++ src/Layout/ImgGallary/ImgGallary.jsx
@@ -1,6 +1,6 @@
+import React from 'react';
 
 const ImgGallary = () => {
-    
     return (
         <div className='container mx-auto my-24'>
             <h1 className='text-center text-4xl font-semibold'>Our Images Gallary</h1>
@@ -29,7 +29,7 @@ const ImgGallary = () => {
                 <div className='w-[370px] h-80 m-2 bg-[#F2F2F2]' data-aos="fade-up" data-aos-duration="1000">
                     <img className='w-full h-full p-2' src="https://m.media-amazon.com/images/I/81GOmHTaSWL._SL1500_.jpg" alt="Gallary Images" />
                 </div>
-                
+
             </div>
         </div>
     );
